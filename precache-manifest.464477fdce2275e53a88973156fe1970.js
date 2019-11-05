d7325f30585cd54ea55cf21126f512f46bc1036c
sagar-metastay <sagar.mahapatra@metastay.com>
2019-11-05 15:24:00 +0530

    deploy

---
+++ precache-manifest.464477fdce2275e53a88973156fe1970.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b2560f86d6df389adb7",
+    "url": "/prism.com/css/app.fe3ccb2f.css"
+  },
+  {
+    "revision": "5e0a207461394de7c04c",
+    "url": "/prism.com/css/chunk-vendors.d4f2f61e.css"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/prism.com/fonts/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/prism.com/fonts/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/prism.com/fonts/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "da4ea5cdfca6b3baab285741f5ccb59f",
+    "url": "/prism.com/fonts/MaterialIcons-Regular.da4ea5cd.ttf"
+  },
+  {
+    "revision": "8200a2e476ec96699052d807377c9d15",
+    "url": "/prism.com/img/analytics.8200a2e4.png"
+  },
+  {
+    "revision": "4739ce62c2b7e4d92464203f4395197d",
+    "url": "/prism.com/img/avatar.4739ce62.svg"
+  },
+  {
+    "revision": "20f824acae3811903e0ccd8efff6730e",
+    "url": "/prism.com/img/bench.20f824ac.png"
+  },
+  {
+    "revision": "b5acd3cf4185c05430b60f3d3cc0c092",
+    "url": "/prism.com/img/change.b5acd3cf.png"
+  },
+  {
+    "revision": "59a4cdcb8684b73765a22dfe67379fef",
+    "url": "/prism.com/img/cost.59a4cdcb.png"
+  },
+  {
+    "revision": "4265c2c348a7526834628a5989ceabdd",
+    "url": "/prism.com/img/custom-app-side.4265c2c3.png"
+  },
+  {
+    "revision": "4faf59cd1bd381389081876bcc3a116a",
+    "url": "/prism.com/img/custom-app.4faf59cd.png"
+  },
+  {
+    "revision": "836666580e58cf13e756883ba8a42f62",
+    "url": "/prism.com/img/email.83666658.png"
+  },
+  {
+    "revision": "17dad23b657ee8104c25084e58a1545e",
+    "url": "/prism.com/img/flexible.17dad23b.png"
+  },
+  {
+    "revision": "5964763c0620aab1288a44f2e3b2a4d2",
+    "url": "/prism.com/img/flow.5964763c.png"
+  },
+  {
+    "revision": "eb3130af4cc1a6bf203f14c24d95346e",
+    "url": "/prism.com/img/focus-analytics.eb3130af.png"
+  },
+  {
+    "revision": "68ef2493491a1a19ced4718d1f08fee4",
+    "url": "/prism.com/img/focus.68ef2493.png"
+  },
+  {
+    "revision": "d6a074a7af0edfee9316d58450ff4932",
+    "url": "/prism.com/img/heading-2.d6a074a7.jpg"
+  },
+  {
+    "revision": "5c7d644de53b85cba32b6b8e9fa96710",
+    "url": "/prism.com/img/location.5c7d644d.png"
+  },
+  {
+    "revision": "3fd62b5d583c9f941dfee82576d25a2b",
+    "url": "/prism.com/img/logo.3fd62b5d.svg"
+  },
+  {
+    "revision": "1d12c2ea2a033fd7258d6874fdd462b5",
+    "url": "/prism.com/img/maintenance.1d12c2ea.png"
+  },
+  {
+    "revision": "da20c1f32aaf3b41b97a649d73a973db",
+    "url": "/prism.com/img/modeler-side.da20c1f3.png"
+  },
+  {
+    "revision": "a4755a5f2cddb46fdfb348cff37d6d4a",
+    "url": "/prism.com/img/modeler.a4755a5f.png"
+  },
+  {
+    "revision": "ef931adeb9b326d5997f6fca16eeb959",
+    "url": "/prism.com/img/monitor-dashboard.ef931ade.png"
+  },
+  {
+    "revision": "ceb7eec8cd21a138414ae16d93f688fe",
+    "url": "/prism.com/img/monitor.ceb7eec8.png"
+  },
+  {
+    "revision": "48dd216245debb15f3b3e0c43e1e301f",
+    "url": "/prism.com/img/performance.48dd2162.png"
+  },
+  {
+    "revision": "9b8edc6ad425cde2347ca704d8158e48",
+    "url": "/prism.com/img/performant.9b8edc6a.png"
+  },
+  {
+    "revision": "f6bc87cf0ed738dd67bb988e14cfb756",
+    "url": "/prism.com/img/platform-side.f6bc87cf.png"
+  },
+  {
+    "revision": "d09d59fea2fa50e7a3f1e76529e21480",
+    "url": "/prism.com/img/platform.d09d59fe.png"
+  },
+  {
+    "revision": "b994f6cda6e5bdf5a3ab4800cddbe8dc",
+    "url": "/prism.com/img/prism_architecture.b994f6cd.png"
+  },
+  {
+    "revision": "46d8a786d91b495aaf911ccd62d5f878",
+    "url": "/prism.com/img/probe.46d8a786.png"
+  },
+  {
+    "revision": "28ed78a6679d5a0e96ed094d8160e2f3",
+    "url": "/prism.com/img/product.28ed78a6.png"
+  },
+  {
+    "revision": "e103d74f9f316192fd3c99b5cc675614",
+    "url": "/prism.com/img/service.e103d74f.png"
+  },
+  {
+    "revision": "2ad490e592e8044f42d7ef8057b16e2b",
+    "url": "/prism.com/img/simple.2ad490e5.png"
+  },
+  {
+    "revision": "ba0c117c2152785880da7357990fa175",
+    "url": "/prism.com/img/task.ba0c117c.png"
+  },
+  {
+    "revision": "1f36182f1f251ab8d80ebf62fa95a05e",
+    "url": "/prism.com/index.html"
+  },
+  {
+    "revision": "de64594796a8c145c22d",
+    "url": "/prism.com/js/about.3e161758.js"
+  },
+  {
+    "revision": "4b2560f86d6df389adb7",
+    "url": "/prism.com/js/app.b6f94577.js"
+  },
+  {
+    "revision": "5e0a207461394de7c04c",
+    "url": "/prism.com/js/chunk-vendors.b801f458.js"
+  },
+  {
+    "revision": "38b7f12079e01c07cab6dab76174bed4",
+    "url": "/prism.com/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/prism.com/robots.txt"
+  }
+]);
